83f0e3ad8a75c8c70f9b37a7127f579d07bc7b85
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-08-25 17:36:42 -0400

    Keep the SARIF output file in the current directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ OUTPUT_ZIP=$(mktemp -t cryticXXXX --suffix=.zip)
 npm install
 crytic-compile . --truffle-version "$2" --export-zip "$OUTPUT_ZIP"
 
-OUTPUT_JSON=$(mktemp -t cryticXXXX --suffix=.json)
+OUTPUT_JSON=$(mktemp cryticXXXX --suffix=.sarif)
 
 (printf '{"zip":"'; base64 -w 0 "$OUTPUT_ZIP"; printf '"}';) | curl -X POST -d @- "$1" > "$OUTPUT_JSON"
 echo "::set-output sarif-path=$OUTPUT_JSON"
